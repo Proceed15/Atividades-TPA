849ced073421a31fb94cee76fae6e1dd2375705a
José Lisboa <104661333+Proceed15@users.noreply.github.com>
2024-05-27 23:08:29 -0300

    Add files via upload

---
+++ Inverte frases 2.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+namespace CShp_StringReverse
+{
+    class Program
+    {
+
+        //Criador: José Roberto Lisboa S. Filho. 1ºano A, DS.
+
+        public static string InverteString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+        static void Main(string[] args)
+        {
+            string texto;
+            Console.WriteLine("Digite uma frase");
+            texto = Console.ReadLine();
+            WriteLine($"Texto Normal   : {texto}");
+            string textoInvertido = new string(texto.Reverse().ToArray());
+            WriteLine($"Texto Invertido: {textoInvertido}");
+        }
+    }
+}
